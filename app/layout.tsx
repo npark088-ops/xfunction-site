08387a03ded256d81b79ccb913a42653aff08507
npark <npark088@gmail.com>
2026-06-27 22:35:02 -0700

    analytics working

--- app/layout.tsx
+++ app/layout.tsx
@@ -11,17 +11,17 @@ export default function RootLayout({
 
       {/* ✅ GOOGLE ANALYTICS */}
       <Script
-        src="https://www.googletagmanager.com/gtag/js?id=G-4LB2D2VX3Q"
+        src="https://www.googletagmanager.com/gtag/js?id=G-36QMZ0WK30"
         strategy="afterInteractive"
       />
 
       <Script id="google-analytics" strategy="afterInteractive">
         {`
-          window.dataLayer = window.dataLayer || [];
-          function gtag(){dataLayer.push(arguments);}
-          gtag('js', new Date());
-          gtag('config', 'G-4LB2D2VX3Q');
-        `}
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+  gtag('config', 'G-36QMZ0WK30');
+`}
       </Script>
 
       <body>
